df2094632e2076a605505699272d685d7be66191
Md Kawsar Islam Yeasin <mdkawsarislam2002@gmail.com>
2023-05-05 15:00:45 +0600

    added SCSS / SASS

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.css";
-import "./styles/tailwind.css";
+import "./styles/tailwind.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
